db25724d33e709baff8a8fe9042917d3deffcc54
Logica <damierny@gmail.com>
2020-01-06 15:16:34 -0600

    Update 120mmCNSTR.lua

--- lua/pewpewbullets/official_weapons/cannons/120mmCNSTR.lua
+++ lua/pewpewbullets/official_weapons/cannons/120mmCNSTR.lua
@@ -29,15 +29,16 @@ BULLET.Gravity = 0
 BULLET.RecoilForce = 800
 BULLET.Spread = 1.5
 BULLET.AffectedBySBGravity = true
+BULLET.ShotCount = 30
 
 -- Damage
 BULLET.DamageType = "PointDamage" -- Look in gcombat_damagecontrol.lua for available damage types
 BULLET.Damage = 20
 
 -- Reloading/Ammo
-BULLET.Reloadtime = 0
-BULLET.Ammo = 30
-BULLET.AmmoReloadtime = 8
+BULLET.Reloadtime = 8
+BULLET.Ammo = 0
+BULLET.AmmoReloadtime = 0
 
 BULLET.EnergyPerShot = 14000
 
